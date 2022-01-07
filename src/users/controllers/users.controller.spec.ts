6a528043561f795ddb3045df7d7a482937acdcf9
Santiago Muñoz <sorduz98@outlook.com>
2022-01-06 20:07:50 -0500

    Fixed tests

--- src/users/controllers/users.controller.spec.ts
+++ src/users/controllers/users.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { UsersService } from '../services/users.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
@@ -7,6 +11,15 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          // how you provide the injection token in a test instance
+          provide: getRepositoryToken(User),
+          // as a class value, Repository needs no generics
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/services/users.service.spec.ts
+++ src/users/services/users.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -6,7 +9,15 @@ describe('UsersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          // how you provide the injection token in a test instance
+          provide: getRepositoryToken(User),
+          // as a class value, Repository needs no generics
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
